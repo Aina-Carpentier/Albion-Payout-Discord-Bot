43a09362d43e7cb9728e12ef1ed6d6656880afb7
Aina <aina.carpentier@etu.univ-smb.fr>
2024-11-22 07:37:07 +0100

    almost done with payout

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 import dotenv
-import discord
-import calc
 import random
+
+import discord
 from discord.ui import Select, View
+
+import calc
+import misc
 
 
 intents = discord.Intents.default()  # Allow the use of custom intents
@@ -22,27 +25,53 @@ async def payout(
     ammount: discord.Option(float, description="The estimated value of the inventory."), 
     repair: discord.Option(float, description="The cost of the repair.")
 ):
-    members = await getplayers(ctx)
-    emojis_animals = "🐵🐻🐼🐭🐮🐯🐰🐱🐲🐳🐴🐶🐷🐸🐹🐺🐑🐗🐘🐙🐛🐜🐝🐞🐠🐡🐢🐣🐥🐌🐍🐎🐒🐔🐩🐫🐧🐬🐿🕷🦁🦄🦃🦀🦂🦇🦊🦌🦍🦏🐀🐁🐂🐃🐄🐅🐆🐇🐈🐉🐊🐋🐏🐐🐓🐕🐖🐤🐨🐪🦆🦛🦡🦢🐟🐦🐽🦅🦈🦉🦎🦓🦒🦔🦕🦗🕊🦝🦜🦚🦧🦮🐕‍🦺🦥🦨🦦🦩"
 
-    select = Select(
-        placeholder = "Select participants...",
-        min_values = nb - 1,
-        max_values = nb - 1,
-        options=[
-        discord.SelectOption(label="Nouveau participant", emoji="✏", description="Ajouter un participant..."),
-    ]) ##disable ?
-    
-    for member in members:
-        print(member)
-        select.options.append(
-            discord.SelectOption(label = member.nick if member.nick is not None else member.name, description=str(member))
-        )
-    
     view = View()
-    view.add_item(select)
 
-    await ctx.respond("Select payout participants :",view=view)
+    members = await getplayers(ctx)
+    emojis_animals = "🐵🐻🐼🐭🐮🐯🐰🐱🐲🐳🐴🐶🐷🐸🐹🐺🐑🐗🐘🐙🐛🐜🐝🐞🐠🐡🐢🐣🐥🐌🐍🐎🐒🐔🐩🐫🐧🐬🐿🕷🦁🦄🦃🦀🦂🦇🦊🦌🦍🦏🐀🐁🐂🐃🐄🐅🐆🐇🐈🐉🐊🐋🐏🐐🐓🐕🐖🐤🐨🐪🦆🦛🦡🦢🐟🐦🐽🦅🦈🦉🦎🦓🦒🦔🦕🦗🕊🦝🦜🦚🦧🦮🦥🦨🦦🦩"
+    count = 1
+
+    async def create_view(count, selected = []):
+        if count >= nb:
+            return
+        
+        view = View()
+
+        select = Select(
+            placeholder = "Select participants...",
+            min_values = 1,
+            max_values = 1,
+            options=[
+            discord.SelectOption(label="Nouveau participant", emoji="✏", description="Ajouter un participant..."),
+        ])
+        
+        for member in members:
+            print(member)
+            if (member.nick is None and member.name not in selected) or (member.nick is not None and member.nick not in selected):
+                select.options.append(
+                    discord.SelectOption(
+                        label = member.nick if member.nick is not None else member.name, 
+                        description=str(member), 
+                        emoji = emojis_animals[random.randint(0,len(emojis_animals)-1)], 
+                        )
+                )
+        
+        async def callback(interaction):
+            await interaction.response.send_message("Please wait...")
+            await interaction.delete_original_response()
+            selected.append(select.values[0])
+            select.disabled = True
+            return await create_view(count + 1, selected) 
+
+
+        select.callback = callback
+        view.add_item(select)
+
+        await ctx.respond(f"Select {misc.ordinal(count)} payout participant :", view = view)
+
+    await create_view(count)
+
 
 
 
