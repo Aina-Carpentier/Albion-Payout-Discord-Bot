f6e4ab7c6c0c0ea0fb8d6061b57d04a2094402b7
Aina CARPENTIER <carpenai@iut-acy.local>
2024-11-22 11:42:02 +0100

    "add participant" choice added

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ bot = discord.Bot(case_insensitive = True, intents = intents)
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
-@bot.slash_command(name="payout", description="Réalise un payout !")
+@bot.slash_command(name="payout", description="Realize a payout with normal taxes!")
 async def payout(
     ctx: discord.ApplicationContext,
     nb: discord.Option(int, description="The number of people to involve in the"),
@@ -32,7 +32,7 @@ async def payout(
     emojis_animals = "🐵🐻🐼🐭🐮🐯🐰🐱🐲🐳🐴🐶🐷🐸🐹🐺🐑🐗🐘🐙🐛🐜🐝🐞🐠🐡🐢🐣🐥🐌🐍🐎🐒🐔🐩🐫🐧🐬🐿🕷🦁🦄🦃🦀🦂🦇🦊🦌🦍🦏🐀🐁🐂🐃🐄🐅🐆🐇🐈🐉🐊🐋🐏🐐🐓🐕🐖🐤🐨🐪🦆🦛🦡🦢🐟🐦🐽🦅🦈🦉🦎🦓🦒🦔🦕🦗🕊🦝🦜🦚🦧🦮🦥🦨🦦🦩"
     count = 1
 
-    async def create_view(count, selected = []):
+    async def create_view(count, selected = [], messages = []):
         if count >= nb:
             return
         
@@ -43,39 +43,41 @@ async def payout(
             min_values = 1,
             max_values = 1,
             options=[
-            discord.SelectOption(label="Nouveau participant", emoji="✏", description="Ajouter un participant..."),
+            discord.SelectOption(label="New participant", emoji="✏", description="Add a participant..."),
         ])
         
         for member in members:
-            print(member)
-            if (member.nick is None and member.name not in selected) or (member.nick is not None and member.nick not in selected):
+            if str(member.id) not in selected:
                 select.options.append(
                     discord.SelectOption(
                         label = member.nick if member.nick is not None else member.name, 
                         description=str(member), 
                         emoji = emojis_animals[random.randint(0,len(emojis_animals)-1)], 
+                        value = str(member.id)
                         )
                 )
         
         async def callback(interaction):
             await interaction.response.send_message("Please wait...")
             await interaction.delete_original_response()
-            selected.append(select.values[0])
-            select.disabled = True
-            return await create_view(count + 1, selected) 
+            if select.values[0] == "New participant":
+                await add_player(ctx)
+                return await create_view(count, selected)
+            else:
+                selected.append(select.values[0])
+                select.disabled = True
+                return await create_view(count + 1, selected) 
 
 
         select.callback = callback
         view.add_item(select)
 
-        await ctx.respond(f"Select {misc.ordinal(count)} payout participant :", view = view)
+        await ctx.respond(f"Select {misc.ordinal(count)} payout participant :", view = view, ephemeral = True)
 
     await create_view(count)
 
 
-
-
-@bot.slash_command(name="payout-premium", description="Réalise un payout avec le premium!")
+@bot.slash_command(name="payout-premium", description="Realize a payout with premium taxes!")
 async def payout_premium(
     ctx: discord.ApplicationContext,
     nb: discord.Option(int, description="The number of people to involve in the"),
@@ -85,14 +87,52 @@ async def payout_premium(
     await ctx.respond(getplayers(ctx))
     await ctx.respond(str(calc.payout_premium(nb, ammount, repair)))
 
+@bot.slash_command(name="add-player")
+async def add_player(
+    ctx: discord.ApplicationContext
+):
+    await ctx.respond("Ping the name of the player you want to add", ephemeral = True)
+    while True:
+        try:
+            message = await bot.wait_for('message', timeout = 15)
+            message = await ctx.fetch_message(message.id)
+
+            tagged = ctx.guild.get_member(int(message.content[2:-1]))
+            await tagged.add_roles(await getrole(ctx))
+            await ctx.respond("The player has been added to the list 👍", ephemeral = True)
+            await message.delete()
+            return
+
+        except TimeoutError:
+            await ctx.respond("Too late...", ephemeral = True)
+            return
+
+
+
+        
+        
+
+    
+def check_ping(author):
+    def inner_check(message):
+        if message.author != author:
+            return False
+
+        content = message.content
+        if content[0:2] != "<@" or content[-1] != ">":
+            return False
+        
+        return True
+    return inner_check
 
 
 async def getplayers(ctx):
-    if discord.utils.get(ctx.guild.roles, name="Albion") is None and discord.utils.get(ctx.guild.roles, name="albion") is None:
-        await ctx.guild.create_role(name="Albion")
-    role = discord.utils.get(ctx.guild.roles, name="Albion") if discord.utils.get(ctx.guild.roles, name="Albion") is not None else discord.utils.get(ctx.guild.roles, name="albion") 
-    
+    role = await getrole(ctx)
+
     return [member for member in role.members if not member.bot] 
+
+async def getrole(ctx):
+    return discord.utils.get(ctx.guild.roles, name="Albion") or discord.utils.get(ctx.guild.roles, name="albion") or await ctx.guild.create_role(name="Albion") or discord.utils.get(ctx.guild.roles, name="Albion")
 
 
 bot.run(os.getenv('TOKEN'))
